e4fcc14f8abe14c74c47b78c8db19ca572a69d0b
Ivan Ukrainskii <devios161@gmail.com>
2016-02-27 09:47:00 +0300

    Initial Commit

---
+++ SecureMyData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecureMyData
+//
+//  Created by MD on 27.02.16.
+//  Copyright © 2016 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
